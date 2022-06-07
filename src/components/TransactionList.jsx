0077e550751a3599906c07be6dcc1ee00d6743e1
ayoub3bidi <mrayoubabidi@gmail.com>
2022-06-07 16:23:00 +0100

    ✅ Adding Hooks & Context API

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+import { GlobalContext } from '../context/GlobalState';
 
-const TransactionList = () => {
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
-    <div>
-        <h3>History</h3>
-        <ul className="list">
-            <li className="minus">
-                Cash<span>-$400</span><button className="delete-btn">x</button>
-            </li>
-        </ul>
-    </div>
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
   )
 }
 
